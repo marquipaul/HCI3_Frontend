da2a46138f2c4d2aea552a97e51e1e936719600f
Paul Marquita <pamarquita@gmail.com>
2019-07-15 13:46:00 +0800

    First Commit

--- src/main.js
+++ src/main.js
@@ -1,9 +1,13 @@
 import Vue from 'vue'
+import './plugins/vuetify'
 import App from './App.vue'
 import router from './router'
 import store from './store'
 import './registerServiceWorker'
+//import './fullpage.scrollHorizontally.min' // Optional. When using fullpage extensions
+import vueScrollto from 'vue-scrollto'
 
+Vue.use(vueScrollto)
 Vue.config.productionTip = false
 
 new Vue({
